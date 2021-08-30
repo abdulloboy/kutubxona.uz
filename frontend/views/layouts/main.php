0e2d9eb2e624d19e1758e62515af88ba345de82f
abdulloboy <89206946+abdulloboy@users.noreply.github.com>
2021-08-31 00:00:02 +0500

    layouts\main.php updated

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -31,7 +31,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'eKutubxona',
+        'brandLabel' => 'Kutubxona.uz',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
